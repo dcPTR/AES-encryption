eba215f9bb8de2f3f35fede8b4db568ec698eb3b
zal <mrocznaklawiatura@gmail.com>
2022-04-16 18:23:04 +0200

    tcp disconnetcinh

--- tcp_handler.py
+++ tcp_handler.py
@@ -1,3 +1,4 @@
+from logging.config import stopListening
 import socket, sys
 
 class TCPHandler:
@@ -8,34 +9,45 @@ class TCPHandler:
         self.ClientPort = clientPort
         self.Client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.Connected = False
+        self.Listening = True
         self.Received = []
+
+    def stop_listen(self):
+        self.Listening = False
+
+    def is_listening(self):
+        return self.Listening
 
     def listen(self):
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.bind(("localhost", self.ServerPort))
         server.listen()
 
-        while True:
-            c, addr = server.accept()
-            msg = ""
-            buf = ""
-            read_msg = False
-            while True:
+        c, addr = server.accept()
+        msg = ""
+        buf = ""
+        read_msg = False
+        c.settimeout(5)
+        while self.Listening:
+            try:
                 data = c.recv(1)
-                if read_msg:
-                    msg += data.decode()
-                else:
-                    buf += data.decode()
+            except:
+                continue
 
-                if buf.startswith(TCPHandler.ID):
-                    read_msg = True
-                    buf = ""
-                elif msg.endswith(TCPHandler.ID):
-                    read_msg = False
-                    self.Received.append(msg[0:-3])
-                    msg = ""
+            if read_msg:
+                msg += data.decode()
+            else:
+                buf += data.decode()
+
+            if buf.startswith(TCPHandler.ID):
+                read_msg = True
+                buf = ""
+            elif msg.endswith(TCPHandler.ID):
+                read_msg = False
+                self.Received.append(msg[0:-3])
+                msg = ""
             
-            c.close()
+        c.close()
     
     def try_connect(self):
         if self.is_connected():
@@ -61,24 +73,21 @@ class TCPHandler:
 
     def recv(self):
         if len(self.Received) > 0:
-            return self.Received.pop(0)
+            return self.Received.pop()
 
         return ""
 
 
 from threading import Thread
 
-def print_receives(tcp):
-    while True:
-        msg = tcp.recv()
-        if len(msg) > 0:
-            print(f"received {msg}")
+def print_receive(tcp):
+    msg = tcp.recv()
+    if len(msg) > 0:
+        print(f"received {msg}")
 
 tcp = TCPHandler(int(input("server port: ")), int(input("client port: ")))
 thread = Thread(target = tcp.listen)
 thread.start()
-thread2 = Thread(target = print_receives, args=(tcp,))
-thread2.start()
 
 while True:
     msg = input("press any key to connect or x to exit")
@@ -87,11 +96,14 @@ while True:
 
     if tcp.try_connect():
         while True:
-            msg = input("type message to send or x to exit")
+            msg = input("type message to send or r to receive or x to exit")
             if msg == "x":
                 break
 
+            if msg == "r":
+                print_receive(tcp)
+
             tcp.send(msg)
 
-thread2.join()
+tcp.stop_listen()
 thread.join()
