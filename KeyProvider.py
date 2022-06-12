30287adad8a76f10f90bf8d629c97cedb08e8e66
dcptr <damiancislo@protonmail.com>
2022-06-12 13:26:59 +0200

    folder path for keys

--- KeyProvider.py
+++ KeyProvider.py
@@ -1,14 +1,15 @@
 from Crypto.PublicKey import RSA
 
+
 class KeyProvider:
-    PRIVATE_KEYS_FILE = "privateKeys.key"
-    PUBLIC_KEYS_FILE = "publicKeys.key"
+    PRIVATE_KEYS_FILE = "keys/private/private_keys.pem"
+    PUBLIC_KEYS_FILE = "keys/public/public_keys.pem"
 
     def __init__(self):
         open(KeyProvider.PRIVATE_KEYS_FILE, 'w').close()
         open(KeyProvider.PUBLIC_KEYS_FILE, 'w').close()
         self.cur_key = 0
-    
+
     def add_many_key_pairs(self, n):
         for i in range(n):
             self.add_key_pair()
@@ -36,7 +37,7 @@ class KeyProvider:
         f_publ = open(KeyProvider.PUBLIC_KEYS_FILE)
         publ_keys = f_publ.read().split("\n\n\n")
 
-        return (priv_keys[self.cur_key], publ_keys[self.cur_key])
+        return priv_keys[self.cur_key], publ_keys[self.cur_key]
 
     def next_key_pair(self):
         self.cur_key += 1
